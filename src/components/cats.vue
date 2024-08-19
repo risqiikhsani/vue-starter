efed794a352c6d5db08aae2a758e86febfe59448
risqiikhsani <risqiikhsani16@gmail.com>
2024-08-19 14:08:34 +0700

    tst

--- src/components/cats.vue
+++ src/components/cats.vue
@@ -1,14 +1,16 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 let id = 0
 
 const newCat = ref('')
 const cats = ref([
-    { id: id++, name: 'Bob' },
-    { id: id++, name: 'Brown' },
-    { id: id++, name: 'White' },
+    { id: id++, name: 'Bob', gone: false },
+    { id: id++, name: 'Brown', gone: true },
+    { id: id++, name: 'White', gone: false },
 ])
+
+const hideGoneCats = ref(false)
 
 function addCat() {
     cats.value.push({ id: id++, name: newCat.value })
@@ -19,19 +21,29 @@ function removeCat(id) {
     cats.value = cats.value.filter((t) => t.id !== id)
 }
 
+const filteredCats = computed(() => {
+    return hideGoneCats.value ? cats.value.filter((t) => !t.gone) : cats.value
+})
+
 </script>
 <template>
     <div>
         <form @submit.prevent="addCat">
-            <input v-model="newCat" />
+            <input v-model="newCat" placeholder="add a new cat" />
             <button>add cat</button>
         </form>
         <ul>
-            <li v-for="cat in cats" :key="cat.id">
+            <!-- <li v-for="cat in cats" :key="cat.id"> -->
+            <li v-for="cat in filteredCats" :key="cat.id">
                 {{ cat.name }}
                 <button @click="removeCat(cat.id)">remove</button>
+
+                {{ cat.gone ? "cat is gone" : "still alive" }}
+                <input type="checkbox" v-model="cat.gone"> is cat gone?
             </li>
         </ul>
+
+        <button @click="hideGoneCats = !hideGoneCats">{{ hideGoneCats ? "Show all" : "Hide Gone cats" }}</button>
 
     </div>
 </template>
