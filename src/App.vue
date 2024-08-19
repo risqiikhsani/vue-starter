93d06b897ffcd21a59e2c9c246575a51122929ef
risqiikhsani <risqiikhsani16@gmail.com>
2024-08-19 13:15:39 +0700

    tst

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { reactive,ref } from 'vue';
+const counter = reactive({
+  count: 0
+})
+const message = ref("hello world")
+
+const titleClass = ref('title')
+
+const count = ref(0)
+
+function increment(){
+  count.value++
+}
+
+function increment2(){
+  counter.count++
+}
+
+</script>
+
+<template>
+  <div>
+    <h1 :class="titleClass">TEST</h1>
+    <h1>{{ message }}</h1>
+    <button @click="increment2">change</button>
+    <h1>count is : {{ counter.count }}</h1>
+    <button @click="increment">change</button>
+    <h1>count = {{ count }}</h1>
+  </div>
+
+</template>
+
+<style scoped>
+.title{
+  color:red
+}
+</style>
